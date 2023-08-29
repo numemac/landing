0482e97a7a35ae430e04871b610a4fcf1fa37fc6
Ryan Augustinsky <ryan@augustynowicz.ca>
2023-08-29 17:33:24 -0400

    hero layout

--- src/HeroSection.js
+++ src/HeroSection.js
@@ -72,7 +72,7 @@ export default class HeroSection extends Component {
         <div className="overflow-hidden">
           <div className="mx-auto max-w-7xl px-6 pb-32 pt-36 sm:pt-60 lg:px-8 lg:pt-32">
             <div className="mx-auto max-w-2xl gap-x-14 lg:mx-0 lg:flex lg:max-w-none lg:items-center">
-              <div className="w-full max-w-xl lg:shrink-0 xl:max-w-2xl">
+              <div className="w-full max-w-xl lg:shrink-0 xl:max-w-xl">
                 <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
                   The federated social network for vegans
                 </h1>
@@ -99,7 +99,7 @@ export default class HeroSection extends Component {
                   </a>
                 </div>
               </div>
-              <div className="mt-14 flex justify-end gap-8 sm:-mt-44 sm:justify-start sm:pl-20 lg:mt-0 lg:pl-0">
+              <div className="mt-8 flex justify-end gap-8 sm:-mt-16 sm:justify-start sm:pl-20 lg:mt-0 lg:pl-0">
                 <div className="ml-auto w-44 flex-none space-y-8 pt-32 sm:ml-0 sm:pt-80 lg:order-last lg:pt-36 xl:order-none xl:pt-80">
                   <div className="relative">
                     <img
