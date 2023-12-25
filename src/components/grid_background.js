a0a643daa61923be744c2078e07af1aa4f221913
Ryan Augustinsky <ryan@augustinsky.ca>
2023-12-24 19:28:43 -0500

    using svg pattern for grid bg now

--- src/components/grid_background.js
+++ src/components/grid_background.js
@@ -1,18 +1,31 @@
 export default function GridBackground() {
 
-    const squares = (amount) => {
-        let result = []
-        for(let i = 0; i < amount; i++) {
-            result.push(<div key={i} className="w-24 lg:w-48 h-24 lg:h-48 border border-gray-300 dark:border-gray-500"></div>)
-        }
-        return result
-    }
-
     return (
-        <div className="absolute -z-10 inset-0 top-0 left-0 w-full h-full opacity-30 [mask-image:radial-gradient(32rem_32rem_at_center,white,transparent)]">
-            <div className="flex flex-row flex-wrap">
-                {squares(100)}
-            </div>
+        <div className="relative isolate -z-10">
+            <svg
+                className="absolute inset-x-0 top-0 -z-10 h-[64rem] w-full stroke-gray-300 dark:stroke-gray-500 opacity-60 [mask-image:radial-gradient(32rem_32rem_at_center,white,transparent)]"
+                aria-hidden="true"
+            >
+                <defs>
+                    <pattern
+                        id="1f932ae7-37de-4c0a-a8b0-a6e3b4d44b84"
+                        width={200}
+                        height={200}
+                        x="50%"
+                        y={-1}
+                        patternUnits="userSpaceOnUse"
+                    >
+                        <path d="M.5 200V.5H200" fill="none" />
+                    </pattern>
+                </defs>
+                <svg x="50%" y={-1} className="overflow-visible fill-gray-50 dark:fill-gray-800">
+                    <path
+                        d="M-200 0h201v201h-201Z M600 0h201v201h-201Z M-400 600h201v201h-201Z M200 800h201v201h-201Z"
+                        strokeWidth={0}
+                    />
+                </svg>
+                <rect width="100%" height="100%" strokeWidth={0} fill="url(#1f932ae7-37de-4c0a-a8b0-a6e3b4d44b84)" />
+            </svg>
         </div>
     );
 }
